68343e4c8e8039b969355bd0663aee205977acad
Karol Guciek <k.guciek@gmail.com>
2014-11-21 11:03:58 +0100

    Matrix calculations

--- apps/jijitai.js
+++ apps/jijitai.js
@@ -6,6 +6,70 @@
 function jijitai(env) {
     "use strict";
 
+    function matrix(h, w) {
+        var m, v = [];
+        (function () {
+            var i;
+            for (i = 0; i < w * h; i += 1) {
+                v.push(0);
+            }
+        }());
+        m = {
+            cell: function (y, x) {
+                return v[y * w + x];
+            },
+            set_cell: function (y, x, val) {
+                val = Number(val);
+                if (!val) { val = 0; }
+                v[y * w + x] = val;
+                return m;
+            },
+            add: function (m2) {
+                if ((w !== m2.width()) || (h !== m2.height())) {
+                    throw "Invalid matrix addition!";
+                }
+                var y, x, ret = matrix(h, w);
+                for (y = 0; y < h; y += 1) {
+                    for (x = 0; x < w; x += 1) {
+                        ret.set_cell(y, x, v[y * w + x] + m2.cell(y, x));
+                    }
+                }
+                return ret;
+            },
+            mult: function (m2) {
+                if (w !== m2.height()) {
+                    throw "Invalid matrix multiplication!";
+                }
+                var s, y, x, i, ret = matrix(h, m2.width());
+                for (y = 0; y < h; y += 1) {
+                    for (x = 0; x < m2.width(); x += 1) {
+                        s = 0;
+                        for (i = 0; i < w; i += 1) {
+                            s += v[y * w + i] * m2.cell(i, x);
+                        }
+                        ret.set_cell(y, x, s);
+                    }
+                }
+                return ret;
+            },
+            width: function () {
+                return w;
+            },
+            height: function () {
+                return h;
+            }
+        };
+        return m;
+    }
+
+    function identity(s) {
+        var i, m = matrix(s, s);
+        for (i = 0; i < s; i += 1) {
+            m.set_cell(i, i, 1);
+        }
+        return m;
+    }
+
     function webgl(canvas) {
         var gl,
             cur_program,
@@ -42,7 +106,7 @@ function jijitai(env) {
             function attribute_init_vec(n, vsize) {
                 var attr = gl.getAttribLocation(prog, n);
                 if (attr < 0) {
-                    throw "getAttribLocation(" + n + ") returned " + attr;
+                    throw "Could not load attribute location!";
                 }
                 gl.enableVertexAttribArray(attr);
                 return {
@@ -57,11 +121,33 @@ function jijitai(env) {
             function uniform_init_vec3(n) {
                 var attr = gl.getUniformLocation(prog, n);
                 if (attr < 0) {
-                    throw "getUniformLocation(" + n + ") returned " + attr;
+                    throw "Could not load uniform variable location!";
                 }
                 return {
                     set_xyz : function (x, y, z) {
                         gl.uniform3f(attr, x, y, z);
+                    }
+                };
+            }
+            function uniform_init_mat(n) {
+                var attr = gl.getUniformLocation(prog, n);
+                if (attr < 0) {
+                    throw "Could not load uniform variable location!";
+                }
+                return {
+                    set_matrix : function (m) {
+                        var x, y, values = [];
+                        for (x = 0; x < m.width(); x += 1) {
+                            for (y = 0; y < m.height(); y += 1) {
+                                values.push(m.cell(y, x));
+                            }
+                        }
+                        values = new Float32Array(values);
+                        if ((m.width() === 4) && (m.height() === 4)) {
+                            gl.uniformMatrix4fv(attr, gl.FALSE, values);
+                        } else {
+                            throw "Invalid matrix size!";
+                        }
                     }
                 };
             }
@@ -101,6 +187,14 @@ function jijitai(env) {
                     activate();
                     uniforms[n] = uniform_init_vec3(n);
                     return uniforms[n];
+                },
+                uniform_mat4: function (n) {
+                    if (uniforms[n]) {
+                        return uniforms[n];
+                    }
+                    activate();
+                    uniforms[n] = uniform_init_mat(n);
+                    return uniforms[n];
                 }
             };
         }
@@ -134,21 +228,11 @@ function jijitai(env) {
         var w = webgl(env.canvas()),
             prog = w.new_program(
                 "attribute vec3 v;" +
-                    "uniform vec3 pos;" +
-                    "uniform vec3 rot;" +
+                    "uniform mat4 rot;" +
+                    "uniform vec3 param;" +
                     "void main() {" +
-                    "    mat3 rotm = mat3(" +
-                    "        1.0, 0.0,         0.0," +
-                    "        0.0, cos(rot.x),  sin(rot.x)," +
-                    "        0.0, -sin(rot.x), cos(rot.x)" +
-                    "    );" +
-                    "    rotm = rotm * mat3(" +
-                    "        cos(rot.y),  0.0, sin(rot.y)," +
-                    "        0.0,         1.0, 0.0," +
-                    "        -sin(rot.y), 0.0, cos(rot.y)" +
-                    "    );" +
-                    "    vec3 p = rotm * (v - pos);" +
-                    "    gl_Position = vec4(p.x * rot.z, p.y, p.z, p.z);" +
+                    "    vec4 p = rot * vec4(v, 1.0);" +
+                    "    gl_Position = vec4(p.x * param.x, p.y, p.z, p.z);" +
                     "}",
                 "void main() {" +
                     "    gl_FragColor = vec4(0.5, 0.5, 1.0, 1.0);" +
@@ -163,23 +247,26 @@ function jijitai(env) {
                         a.push(x);
                         a.push(0);
                         a.push(z);
-                        a.push(x+1);
+                        a.push(x + 1);
                         a.push(0);
                         a.push(z);
                         a.push(x);
                         a.push(0);
-                        a.push(z+1);
+                        a.push(z + 1);
                     }
                 }
                 return w.new_buffer_float32(a);
-            })();
+            }());
         prog.attribute_vec3("v").set_buffer(buf);
         return {
             redraw: function (cam) {
                 w.clear();
-                prog.uniform_vec3("pos").set_xyz(cam.x, cam.y, cam.z);
-                prog.uniform_vec3("rot").set_xyz(cam.rx, cam.ry,
-                        env.canvas().height / env.canvas().width);
+                prog.uniform_mat4("rot").set_matrix(cam);
+                prog.uniform_vec3("param").set_xyz(
+                    env.canvas().height / env.canvas().width,
+                    0,
+                    0
+                );
                 prog.draw_triangles();
             },
             step: function () {
@@ -193,16 +280,31 @@ function jijitai(env) {
             drawn = false,
             mousex = -1,
             mousey = -1,
-            camx = 0,
-            camy = 1,
-            camz = 0,
-            speedx = 0,
-            speedz = 0;
+            cam_pos = matrix(4, 1),
+            movement = matrix(4, 1),
+            key_pressed = {};
+
+        cam_pos.set_cell(1, 0, 1);
+
+        function m_rotate(r, i) {
+            var i1 = (i + 1) % 3, i2 = (i + 2) % 3;
+            return identity(4).
+                    set_cell(i1, i1, Math.cos(r)).
+                    set_cell(i1, i2, Math.sin(r)).
+                    set_cell(i2, i1, -Math.sin(r)).
+                    set_cell(i2, i2, Math.cos(r));
+        }
+
+        function m_move_minus(v) {
+            return identity(4).
+                    set_cell(0, 3, -v.cell(0, 0)).
+                    set_cell(1, 3, -v.cell(1, 0)).
+                    set_cell(2, 3, -v.cell(2, 0));
+        }
 
         function onframe() {
             var mx = env.mouse().getX(),
-                my = env.mouse().getY(),
-                cam;
+                my = env.mouse().getY();
             if ((mx < 0) && (my < 0)) {
                 mx = Math.floor(env.canvas().width * 0.5);
                 my = Math.floor(env.canvas().height * 0.5);
@@ -212,23 +314,15 @@ function jijitai(env) {
                 mousey = my;
                 drawn = false;
             }
-            if ((speedx !== 0) || (speedz !== 0)) {
-                drawn = false;
-            }
             if (!drawn) {
                 drawn = true;
-                cam = {
-                    x: camx,
-                    y: camy,
-                    z: camz,
-                    rx: 3 * (0.5 - (mousey / env.canvas().height)),
-                    ry: 9 * ((mousex / env.canvas().width) - 0.5)
-                };
-                camx += speedz * Math.sin(cam.ry) * 0.03;
-                camz += speedz * Math.cos(cam.ry) * 0.03;
-                camx += speedx * Math.cos(cam.ry) * 0.03;
-                camz -= speedx * Math.sin(cam.ry) * 0.03;
-                v.redraw(cam);
+                mx = 3 * (0.5 - (mousey / env.canvas().height));
+                my = 11 * ((mousex / env.canvas().width) - 0.5);
+                cam_pos = cam_pos.add(m_rotate(-my, 1).mult(m_rotate(mx, 0)).
+                        mult(movement));
+                movement = matrix(4, 1);
+                v.redraw(m_rotate(-mx, 0).mult(m_rotate(my, 1)).
+                        mult(m_move_minus(cam_pos)));
             }
             env.runOnNextFrame(onframe);
         }
@@ -249,17 +343,34 @@ function jijitai(env) {
         });
 
         document.body.onkeydown = env.eventHandler(function (ev) {
-            speedx = 0;
-            speedz = 0;
-            if (ev.keyCode === 87) { speedz = 1; }
-            if (ev.keyCode === 83) { speedz = -1; }
-            if (ev.keyCode === 65) { speedx = -1; }
-            if (ev.keyCode === 68) { speedx = 1; }
+            if (ev.keyCode) {
+                key_pressed[ev.keyCode] = true;
+            }
         });
         document.body.onkeyup = env.eventHandler(function (ev) {
-            speedx = 0;
-            speedz = 0;
+            if (ev.keyCode) {
+                key_pressed[ev.keyCode] = undefined;
+            }
         });
+        setInterval(env.eventHandler(function () {
+            var speed = 0.05;
+            if (key_pressed[87]) {
+                movement.set_cell(2, 0, movement.cell(2, 0) + speed);
+                drawn = false;
+            }
+            if (key_pressed[83]) {
+                movement.set_cell(2, 0, movement.cell(2, 0) - speed);
+                drawn = false;
+            }
+            if (key_pressed[65]) {
+                movement.set_cell(0, 0, movement.cell(0, 0) - speed);
+                drawn = false;
+            }
+            if (key_pressed[68]) {
+                movement.set_cell(0, 0, movement.cell(0, 0) + speed);
+                drawn = false;
+            }
+        }), 30);
 
         env.menu().addSubmenu("Fractal").
             addLink("Mandelbulb", "#mandelbulb").
